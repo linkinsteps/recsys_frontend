c4ad925736d88a14946593931845d0639e3a95f6
DucDHM <ducdhm@gmail.com>
2015-04-13 00:36:29 +0700

    First demo about getting rec

--- assets/js/rs.js
+++ assets/js/rs.js
@@ -30,15 +30,43 @@ rs.getSuggestions = function () {
 	});
 };
 
-rs.doAjax = function () {
-	var target = $('div[data-rs=true]');
-	
+rs.renderRecs = function (recs) {
+	var htmlStr = '';
+
+	for (var i = 0; i < recs.length; i++) {
+		var rec = recs[i];
+
+        htmlStr += '<div class="media">';
+        htmlStr += '    <div class="media-body">';
+        htmlStr += '        <h4 class="media-heading">';
+        htmlStr += '        	<a href="' + rec.url + '">' + rec.title + '</a>';
+        htmlStr += '        </h4>';
+        htmlStr += '        <p>' + rec.meta + '</p>';
+        htmlStr += '    </div>';
+        htmlStr += '</div>';
+	}
+
+	return htmlStr;
+}
+
+function getrecs(resp) {
+	var recsStr = rs.renderRecs(resp.recs);
+
+	$(function () {
+		$('div[data-rs=true]').each(function () {
+			var target = $(this);
+
+			target.html(recsStr);
+		});
+	});
+}
+
+
+rs.doAjax = function () {	
 	$.ajax({
-        url: 'http://demo.richanchors.com/recs/?f=json&demo=1&v=fuck',
+        url: 'http://richanchors.com:3333/recs/?f=jsonp&demo=1&v=fuck&callback=getrecs',
         dataType: 'jsonp',
-        success: function(resp) {
-            console.log(resp);
-        }
+        callback: 'getrecs'
     });
 };
 
