e0d15235e8b55b58292d74ba67f8cdbe0ec0d3d9
DucDHM <ducdhm@gmail.com>
2015-08-01 00:41:36 +0700

    Handshake

---
+++ rs-cdn/public/rs-cdn/rs.handshake.js
@@ -0,0 +1,59 @@
+(function (rs, Cookies) {
+    /**
+     * Get random string include digit and number with total length is 32
+     * @return {String}
+     */
+    rs.getRandomString = function () {
+        return (
+            Math.random().toString(36).substr(2, 8) + 
+            Math.random().toString(36).substr(2, 8) + 
+            Math.random().toString(36).substr(2, 8) + 
+            Math.random().toString(36).substr(2, 8)
+        );
+    };
+
+    rs.UID_NAME = {
+        RS: 'rs_uid_rs'
+    };
+
+    /**
+     * Get url which will post message to
+     * @return {String}
+     */
+    rs.getPostMessageUrl = function () {
+        var regex = new RegExp('[\\?&]url=([^&#]*)');
+        var results = regex.exec(window.location.href);
+
+        if (results == null) {
+            return '';
+        } else {
+            return unescape(results[1]);
+        }
+    };
+
+    /**
+     * Let hand shake with other site!!!
+     */
+    rs.handshake = function () {
+        var postUrl = rs.getPostMessageUrl();
+        if (postUrl) {
+            var globalUID = Cookies.get(rs.UID_NAME.RS);
+
+            if (!globalUID) {
+                globalUID = rs.getRandomString();
+                Cookies.set(rs.UID_NAME.RS, rs.siteCookie, {
+                    path: '/',
+                    expires: 999
+                });
+            }
+
+            var postData = {};
+            postData[rs.UID_NAME.RS] = globalUID;
+
+            window.postMessage(postData, postUrl);
+        }
+    };
+
+    rs.handshake();
+
+})(rs = window.rs || {}, Cookies);
